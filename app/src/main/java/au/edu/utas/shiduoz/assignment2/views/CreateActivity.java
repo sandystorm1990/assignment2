e8dc5b9e10cec2f7cd9bf89e65307af44f6799d6
sandystorm1990 <1135444595@qq.com>
2019-05-20 14:07:19 +1000

    mood item select listener

--- app/src/main/java/au/edu/utas/shiduoz/assignment2/views/CreateActivity.java
+++ app/src/main/java/au/edu/utas/shiduoz/assignment2/views/CreateActivity.java
@@ -21,7 +21,7 @@ import au.edu.utas.shiduoz.assignment2.R;
 public class CreateActivity extends AppCompatActivity {
 
     Button addBtn, saveGoBtn;
-    TextView selectDate, closeBtn;
+    TextView dateInputCreate, selectDate, closeBtn;
     int mYear, mMonth, mDay;
     final int DATE_DIALOG = 1;
     final SpannableStringBuilder style = new SpannableStringBuilder();
@@ -29,7 +29,8 @@ public class CreateActivity extends AppCompatActivity {
     // mood level
     TextView moodLevel1, moodLevel2, moodLevel3,moodLevel4,moodLevel5;
     // moods
-    TextView moodHappy, moodSmile, moodAngry;
+    TextView moodHappy, moodSmile, moodAngry, moodBored, moodConfused,moodEmbarrassed, moodIll,
+            moodSmart,moodSecret,moodNerd, moodSuspicious, moodKissing, moodLove, moodCrying, moodMad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +38,8 @@ public class CreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create);
 
         // date select
-        selectDate = findViewById(R.id.dateInputCreate);
+        selectDate = findViewById(R.id.dateSelectCreate);
+        dateInputCreate = findViewById(R.id.dateInputCreate);
         final Calendar ca = Calendar.getInstance();
         mYear = ca.get(Calendar.YEAR);
         mMonth = ca.get(Calendar.MONTH);
@@ -100,7 +102,7 @@ public class CreateActivity extends AppCompatActivity {
         //Date selectDate = sdf.parse(mYear+"-"+mMonth+"-"+mDay);
         String t2 = sdf.format(selectedDate);
         //selectDate.setText(Html.fromHtml("<u>"+t2+"</u>"));
-        selectDate.setText(new StringBuffer().append(t2));
+        dateInputCreate.setText(new StringBuffer().append(t2));
 //        String str = mDay+"/"+mMonth+1+"/"+mYear;
 //        today.setText(Html.fromHtml(str));
     }
@@ -119,6 +121,11 @@ public class CreateActivity extends AppCompatActivity {
     private void selectMoodLevel() {
         // setting tapped mood level
         moodLevel1 = findViewById(R.id.moodLevel1);
+        moodLevel2 = findViewById(R.id.moodLevel2);
+        moodLevel3 = findViewById(R.id.moodLevel3);
+        moodLevel4 = findViewById(R.id.moodLevel4);
+        moodLevel5 = findViewById(R.id.moodLevel5);
+
         moodLevel1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -126,7 +133,7 @@ public class CreateActivity extends AppCompatActivity {
                 moodLevel1.setBackgroundColor(getResources().getColor(R.color.moodLevelBg));
             }
         });
-        moodLevel2 = findViewById(R.id.moodLevel2);
+
         moodLevel2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -134,7 +141,7 @@ public class CreateActivity extends AppCompatActivity {
                 moodLevel2.setBackgroundColor(getResources().getColor(R.color.moodLevelBg));
             }
         });
-        moodLevel3 = findViewById(R.id.moodLevel3);
+
         moodLevel3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -142,7 +149,7 @@ public class CreateActivity extends AppCompatActivity {
                 moodLevel3.setBackgroundColor(getResources().getColor(R.color.moodLevelBg));
             }
         });
-        moodLevel4 = findViewById(R.id.moodLevel4);
+
         moodLevel4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -150,7 +157,7 @@ public class CreateActivity extends AppCompatActivity {
                 moodLevel4.setBackgroundColor(getResources().getColor(R.color.moodLevelBg));
             }
         });
-        moodLevel5 = findViewById(R.id.moodLevel5);
+
         moodLevel5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -171,28 +178,158 @@ public class CreateActivity extends AppCompatActivity {
     //
     private void selectMood() {
         moodHappy = findViewById(R.id.moodHappy);
+        moodSmile = findViewById(R.id.moodSmile);
+        moodAngry = findViewById(R.id.moodAngry);
+        moodBored = findViewById(R.id.moodBored);
+        moodConfused = findViewById(R.id.moodConfused);
+        moodCrying = findViewById(R.id.moodCrying);
+        moodEmbarrassed = findViewById(R.id.moodEmbarrassed);
+        moodMad = findViewById(R.id.moodMad);
+        moodLove = findViewById(R.id.moodLove);
+        moodKissing = findViewById(R.id.moodKissing);
+        moodSuspicious = findViewById(R.id.moodSuspicious);
+        moodIll = findViewById(R.id.moodIll);
+        moodSmart = findViewById(R.id.moodSmart);
+        moodNerd = findViewById(R.id.moodNerd);
+        moodSecret = findViewById(R.id.moodSecret);
+
         moodHappy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clearMoodBgColor();
                 moodHappy.setBackgroundColor(getResources().getColor(R.color.moodBg));
             }
         });
 
-        moodSmile = findViewById(R.id.moodSmile);
         moodSmile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clearMoodBgColor();
                 moodSmile.setBackgroundColor(getResources().getColor(R.color.moodBg));
             }
         });
 
-        moodAngry = findViewById(R.id.moodAngry);
         moodAngry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                clearMoodBgColor();
                 moodAngry.setBackgroundColor(getResources().getColor(R.color.moodBg));
             }
         });
 
+        moodBored.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodBored.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodConfused.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodConfused.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodCrying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodCrying.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodEmbarrassed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodEmbarrassed.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodMad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodMad.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+
+        moodLove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodLove.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodKissing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodKissing.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+
+        moodSuspicious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodSuspicious.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodIll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodIll.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodSmart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodSmart.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+
+        moodNerd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodNerd.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+        moodSecret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearMoodBgColor();
+                moodSecret.setBackgroundColor(getResources().getColor(R.color.moodBg));
+            }
+        });
+    }
+
+    private void clearMoodBgColor() {
+        moodAngry.setBackgroundColor(Color.TRANSPARENT);
+        moodHappy.setBackgroundColor(Color.TRANSPARENT);
+        moodSmile.setBackgroundColor(Color.TRANSPARENT);
+        moodBored.setBackgroundColor(Color.TRANSPARENT);
+        moodConfused.setBackgroundColor(Color.TRANSPARENT);
+        moodCrying.setBackgroundColor(Color.TRANSPARENT);
+        moodEmbarrassed.setBackgroundColor(Color.TRANSPARENT);
+        moodMad.setBackgroundColor(Color.TRANSPARENT);
+        moodLove.setBackgroundColor(Color.TRANSPARENT);
+        moodKissing.setBackgroundColor(Color.TRANSPARENT);
+        moodNerd.setBackgroundColor(Color.TRANSPARENT);
+        moodSecret.setBackgroundColor(Color.TRANSPARENT);
+        moodSuspicious.setBackgroundColor(Color.TRANSPARENT);
+        moodIll.setBackgroundColor(Color.TRANSPARENT);
+        moodSmart.setBackgroundColor(Color.TRANSPARENT);
     }
 }
